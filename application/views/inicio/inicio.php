0e7643c32d1677e5225fbce6940c6660f6f3c5d1
Carlos Escutia <carlosescutia@gmail.com>
2022-02-12 20:57:48 -0600

    Se implementa acceso por usuario y contraseña, machote de encabezado y pie para todas las paginas, bitácora de actividad; se renombran las tablas: accesos a opciones del sistema, accesos_rol a accesos_sistema ;se eliminan controlador y vista de ejemplo 'welcome'; se configura carga automática de librería database, driver session y helper url.

---
+++ application/views/inicio/inicio.php
@@ -0,0 +1,10 @@
+<main role="main" class="ml-sm-auto px-4">
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h2>Inicio</h2>
+    </div>
+    <div class="row">
+    </div>
+
+    <hr />
+
+</main>
