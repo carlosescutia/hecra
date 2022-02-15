e312f4fd88639bff5e6ba78041203b59008df454
Carlos Escutia <cescutia@guanajuato.gob.mx>
2022-02-15 15:45:05 -0600

    Se cambia el color de fondo de los botones de catalogos al pasar el raton encima.

--- application/views/catalogos/usuarios/boton.php
+++ application/views/catalogos/usuarios/boton.php
@@ -1,5 +1,5 @@
 <a href="<?= base_url() ?>usuarios" style="text-decoration: none; color: black">
-	<div class="card">
+	<div class="card btn-catalogo">
 		<div class="card-body">
 			<div class="row">
 				<div class="col-md-12">
