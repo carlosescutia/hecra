a4bbbec6e46481f728d03f7ea015f39fe171647d
Carlos Escutia <cescutia@guanajuato.gob.mx>
2022-02-15 15:50:42 -0600

    Se agrega catalogo de Roles.

---
+++ application/controllers/Roles.php
@@ -0,0 +1,121 @@
+<?php
+class Roles extends CI_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('url');
+        $this->load->model('roles_model');
+        $this->load->model('accesos_sistema_model');
+    }
+
+    public function index()
+    {
+        if ($this->session->userdata('logueado')) {
+            $cve_rol = $this->session->userdata('cve_rol');
+            $data['cve_usuario'] = $this->session->userdata('cve_usuario');
+            $data['cve_dependencia'] = $this->session->userdata('cve_dependencia');
+            $data['nom_dependencia'] = $this->session->userdata('nom_dependencia');
+            $data['cve_rol'] = $cve_rol;
+            $data['nom_usuario'] = $this->session->userdata('nom_usuario');
+            $data['error'] = $this->session->flashdata('error');
+            $data['accesos_sistema_rol'] = explode(',', $this->accesos_sistema_model->get_accesos_sistema_rol($cve_rol)['accesos']);
+            
+            if ($cve_rol != 'adm') {
+                redirect('inicio');
+            }
+
+            $data['roles'] = $this->roles_model->get_roles();
+
+            $this->load->view('templates/header', $data);
+            $this->load->view('catalogos/roles/lista', $data);
+            $this->load->view('templates/footer');
+        } else {
+            redirect('inicio/login');
+        }
+    }
+
+    public function detalle($cve_rol)
+    {
+        if ($this->session->userdata('logueado')) {
+            $cve_rol = $this->session->userdata('cve_rol');
+            $data['cve_usuario'] = $this->session->userdata('cve_usuario');
+            $data['cve_dependencia'] = $this->session->userdata('cve_dependencia');
+            $data['nom_dependencia'] = $this->session->userdata('nom_dependencia');
+            $data['cve_rol'] = $cve_rol;
+            $data['nom_usuario'] = $this->session->userdata('nom_usuario');
+            $data['error'] = $this->session->flashdata('error');
+            $data['accesos_sistema_rol'] = explode(',', $this->accesos_sistema_model->get_accesos_sistema_rol($cve_rol)['accesos']);
+
+            if ($cve_rol != 'adm') {
+                redirect('inicio');
+            }
+
+            $data['roles'] = $this->roles_model->get_rol($cve_rol);
+
+            $this->load->view('templates/header', $data);
+            $this->load->view('catalogos/roles/detalle', $data);
+            $this->load->view('templates/footer');
+        } else {
+            redirect('inicio/login');
+        }
+    }
+
+    public function nuevo()
+    {
+        if ($this->session->userdata('logueado')) {
+            $cve_rol = $this->session->userdata('cve_rol');
+            $data['cve_usuario'] = $this->session->userdata('cve_usuario');
+            $data['cve_dependencia'] = $this->session->userdata('cve_dependencia');
+            $data['nom_dependencia'] = $this->session->userdata('nom_dependencia');
+            $data['cve_rol'] = $cve_rol;
+            $data['nom_usuario'] = $this->session->userdata('nom_usuario');
+            $data['error'] = $this->session->flashdata('error');
+            $data['accesos_sistema_rol'] = explode(',', $this->accesos_sistema_model->get_accesos_sistema_rol($cve_rol)['accesos']);
+
+            if ($cve_rol != 'adm') {
+                redirect('inicio');
+            }
+
+            $this->load->view('templates/header', $data);
+            $this->load->view('catalogos/roles/nuevo', $data);
+            $this->load->view('templates/footer');
+        } else {
+            redirect('inicio/login');
+        }
+    }
+
+    public function guardar($cve_rol=null)
+    {
+        if ($this->session->userdata('logueado')) {
+
+            $roles = $this->input->post();
+            if ($roles) {
+                $data = array(
+                    'nom_rol' => empty($roles['nom_rol']) ? null : $roles['nom_rol']
+                );
+                $this->roles_model->guardar($data, $cve_rol);
+            }
+            redirect('roles');
+
+        } else {
+            redirect('inicio/login');
+        }
+    }
+
+    public function eliminar($cve_rol)
+    {
+        if ($this->session->userdata('logueado')) {
+
+            $this->roles_model->eliminar($cve_rol);
+            redirect('roles');
+
+        } else {
+            redirect('inicio/login');
+        }
+    }
+
+}
+
+
+
