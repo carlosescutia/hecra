f1117c3b36142941b17abbf72559814dbbca79de
Carlos Escutia <carlosescutia@gmail.com>
2022-05-04 14:39:55 -0500

    Se ajusta imagen a nueva identidad grafica mayo 2022; se ajustan elementos del pie de pagina.

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,20 +1,34 @@
-            <footer class="masfoot d-print-none ml-sm-auto px-4">
-                <div class="address">
-                    Instituto de Planeación, Estadística y Geografía del Estado de Guanajuato<br />
-                    Blvd Mineral de Valenciana 351, Puerto Interior de Gto. C.P 36275  <br>
-                    Lunes a Viernes de 9:00 a.m. a 5:00 p.m.<br>
-                    Correo de atención iplaneg@guanajuato.gob.mx  <br>
-                    Silao de la Victoria, Gto. México.
-                </div>
-                <div class="social">
-                    <a href="https://www.facebook.com/Iplaneg/" target="_blank" title="Facebook"><span class="fab fa-facebook-f"></span></a>
-                    <a href="https://twitter.com/iplaneg" target="_blank" title="Twitter"><span class="fab fa-twitter"></span></a>
-                </div>
-                <div class="address">
-                    <p>© 2020 Gobierno del Estado de Guanajuato, Derechos Reservados</p>
-                </div>
-            </footer>
         </div>
+        <footer class="masfoot d-print-none ml-sm-auto px-4">
+            <div class="row">
+                <div class="col-md-1">
+                </div>
+                <div class="col-md-2">
+                    <img class="img-fluid mb-3" src="<?=base_url()?>img/gto_iplaneg.png" style="max-height: 50px; padding-top:10px;">
+                </div>
+                <div class="col-md-3">
+                    <p class="section_title">ATENCIÓN CIUDADANA</p>
+                    <p><i class="fa fa-phone"></i> 472 103 7700</p>
+                    <p><i class="fa fa-envelope"></i> iplaneg@guanajuato.gob.mx</p>
+                </div>
+                <div class="col-md-1">
+                    <img class="img-fluid mb-3" src="<?=base_url()?>img/logo-gl.svg" style="max-height: 100px; padding-top:10px; ">
+                </div>
+                <div class="col-md-3">
+                    <p class="section_title">NUESTRA SEDE</p>
+                    <a href="https://www.google.com/maps/place/Instituto+de+Planeaci%C3%B3n,+Estad%C3%ADstica+y+Geograf%C3%ADa+del+Estado+de+Guanajuato+-+IPLANEG/@21.0079503,-101.5081362,17z/data=!3m1!4b1!4m5!3m4!1s0x842b99ebb0c8e155:0xd74201edd922fff6!8m2!3d21.0079336!4d-101.5059451" target="_blank">
+                        <p><i class="fa fa-map-marker"></i> Blvd Mineral de Valenciana 351</p>
+                        <p>Puerto interior de Gto. C.P. 36275</p>
+                        <p>Silao de la Victoria, Gto.</p>
+                    </a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <a href="https://transparencia.guanajuato.gob.mx/conocenos/aviso_privacidad.php" target="_blank"><p>Aviso legal © 2022 Gobierno del Estado de Guanajuato</p></a>
+                </div>
+            </div>
+        </footer>
     </body>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.9.0/feather.min.js"></script>
 	<script>
