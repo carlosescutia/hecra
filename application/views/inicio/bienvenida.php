6c25fcacc9d51a8769153429dcfa9a9b960cf655
Carlos Escutia <cescutia@guanajuato.gob.mx>
2022-06-26 00:23:05 -0500

    Se modifican mensajes en sección Inicio.

--- application/views/inicio/bienvenida.php
+++ application/views/inicio/bienvenida.php
@@ -1,11 +1,20 @@
 <h5>Herramienta para la evaluación de la calidad de los registros administrativos (HECRA)</h5>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi
-tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+
+<p>La HECRA es un instrumento de <strong>autodiagnóstico y autoevaluación de la calidad de los registros administrativos</strong> que son susceptibles de ser aprovechados para la generación de información estadística y geográfica, no solo de los utilizados para fines de evaluación de instrumentos de planeación, sino también aquellos derivados del quehacer gubernamental. Es importante señalar que el uso de la HECRA no significa que el IPLANEG otorgue el aval de calidad a algún registro administrativo, debido a que únicamente es un instrumento de autodiagnóstico diseñado para identificar las áreas de oportunidad y elaborar un plan de acción con el fin de mejorar la calidad de sus registros.</p>
+
+<p>Objetivos:</p>
+
+<ul>
+    <li>Evaluar y mejorar la calidad de los Registros Administrativos a ser usados con fines estadísticos.</li>
+    <li>Detectar problemas y oportunidades de mejora de los registros analizados.</li>
+    <li>Identificar y analizar las causas de los problemas.</li>
+    <li>Implementar planes de acciones correctivas y preventivas.</li>
+</ul>
 
 <hr>
 
 <h5>Instrucciones para el llenado:</h5>
 
-<p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.</p>
-<p>Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae.</p>
+<p>En la sección de Cuestionarios, están habilitados los cuestionarios HECRA para aplicar a los registros administrativos solicitados. Así como el cuestionario para identificar los registros administrativos cuando partimos de los indicadores del programa de gobierno. En cada uno siga las instrucciones indicadas y consulte la  guía de llenado en cada pregunta.</p>
+
+<p>En caso de requerir asesoría adicional, favor de contactarse con Mario Hernández, al teléfono 4721037700 extensión 107, o al correo: mhernanm@guanajuato.gob.mx</p>
